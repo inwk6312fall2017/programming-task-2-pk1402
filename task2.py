d5a01b396236be5af94020bf7b82c5ad29d099b7
pk1402 <praveen.kumarporumalla@dal.ca>
2017-10-02 12:58:29 -0300

    created a class with name crime

--- task2.py
+++ task2.py
@@ -1,9 +1,15 @@
 ##print("sucessfully initiated git hub")
 
-with open('crime.csv') as myfile:
+with open(crime_csv.txt,'r') as myfile:
 	data=myfile.readline()
 	list=data.split("")
 	word=list[0]
+class crime:
+	def  init(self,crime_type,crime_id,crime_count):
+		self.crime_type=crime_type
+		self.crime_id=crime_id
+		self.crime_count=crime_count
+
 
 
 
