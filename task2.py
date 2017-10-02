f5b1adf80f78b817522be7a8bac67981d96b4144
pk1402 <praveen.kumarporumalla@dal.ca>
2017-10-02 12:47:10 -0300

    opened the file

--- task2.py
+++ task2.py
@@ -1,3 +1,11 @@
-print("sucessfully initiated git hub")
+##print("sucessfully initiated git hub")
+
+with open('crime.csv') as myfile:
+	data=myfile.readline()
+	list=data.split("")
+	word=list[0]
+
+
+
 
 
